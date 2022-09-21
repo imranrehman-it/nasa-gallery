8c087cb971175cacc02d8810508d399c9c2d86c9
Imran Rehman <imran.rehman@mail.utoronto.ca>
2022-09-21 14:33:47 -0400

    created image of the day section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./index.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="iotd">
+        <img
+          className="iotd iotd-img"
+          src="http://www.nasa.gov/sites/default/files/thumbnails/image/web_first_images_release.png"
+        ></img>
+        <text className="iotd-text">Nasa image Of The Day</text>
+      </div>
+      <div className="now-trending"></div>
     </div>
   );
 }
